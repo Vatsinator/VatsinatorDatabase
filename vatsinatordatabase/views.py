fb91eeab5fac881ca5e0764776745bf4bdbea084
Garrappachc <michal@garapich.pl>
2014-05-15 12:58:36 +0200

    Added database statistics

--- vatsinatordatabase/views.py
+++ vatsinatordatabase/views.py
@@ -1,5 +1,9 @@
 from annoying.decorators import render_to
 
+from airlines.models import Airline
+from airports.models import Airport
+from commits.models import Commit
+
 
 @render_to('index.html')
 def index(request):
@@ -8,4 +12,8 @@ def index(request):
     @param request: The HttpRequest
     @return: The HttpResponse
     """
-    return {}
+    return {
+        'airport_count': Airport.objects.count(),
+        'airline_count': Airline.objects.count(),
+        'last_commit': Commit.objects.filter(status='AC').order_by('-timestamp')[0]
+    }
